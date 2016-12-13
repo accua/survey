1b2b7654cefa0ae97cffaf6b15e216a6b00471bb
Andrew Accuardi and Skye Atwood <skye@>
2016-12-13 15:45:35 -0800

    add update and delete functionality to Survey and Question classes.

--- app.rb
+++ app.rb
@@ -19,17 +19,58 @@ post ('/survey/new') do
   erb :index
 end
 
-get ('/surveys/:id') do
+get('/surveys/:id/edit') do
+  @survey = Survey.find(params[:id].to_i)
+  erb :survey_edit
+end
+
+patch('/surveys/:id') do
+  title = params[:title]
+  @survey = Survey.find(params[:id].to_i)
+  @survey.update({title: title})
+  @surveys = Survey.all
+  erb :index
+end
+
+delete('/surveys/:id') do
+  @survey_id = Survey.find(params[:id].to_i)
+  @survey_id.delete
+  @surveys = Survey.all
+  erb :index
+end
+
+get('/surveys/:id') do
   @survey = Survey.find(params[:id].to_i)
   erb :survey
 end
 
-post ('/surveys/:id/questions') do
+post('/surveys/:id/questions') do
   question = params[:question]
   id_survey = params[:id_survey].to_i
   Question.create({description: question, survey_id: id_survey})
   @survey = Survey.find(id_survey)
   erb :survey
+end
+
+get('/questions/:id/edit') do
+  @question = Question.find(params[:id].to_i)
+  erb :question_edit
+end
+
+patch('/questions/:id') do
+  description = params[:description]
+  @survey = Survey.find(params[:survey_id].to_i)
+  @question = Question.find(params[:id].to_i)
+  @question.update({description: description})
+  @questions = Question.all
+  erb :survey
+end
+
+delete('/questions/:id') do
+  @question = Question.find(params[:id].to_i)
+  @survey = Survey.find(@question.survey_id)
+  @question.delete
+  erb(:survey)
 end
 
 get ('/question/:id') do
