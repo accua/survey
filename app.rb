98ee21e9480c968048ddb0b6d25c064802527a96
Andrew Accuardi and Skye Atwood <skye@>
2016-12-13 11:58:13 -0800

    finishing front-end. add answer column to question class.

--- app.rb
+++ app.rb
@@ -13,7 +13,39 @@ end
 
 post ('/survey/new') do
   title = params[:title]
-  Survey.create({title: "top food carts!"})
+  Survey.create({title: title})
   @surveys = Survey.all
   erb :index
+end
+
+get ('/surveys/:id') do
+  @survey = Survey.find(params[:id].to_i)
+  erb :survey
+end
+
+post ('/surveys/:id/questions') do
+  question = params[:question]
+  id_survey = params[:id_survey]
+  Question.create({description: question, survey_id: id_survey})
+  @survey = Survey.find(id_survey)
+  erb :survey
+end
+
+get ('/question/:id') do
+  @question = Question.find(params[:id].to_i)
+  erb :question
+end
+
+post ('/questions/:id/') do
+  answer = params[:answer]
+  id_question = params[:id_question]
+  Question.create({answer: answer})
+  @question = Question.find(id_question)
+  erb :question
+end
+
+get ('/method') do
+  Survey.all.each {|survey| survey.destroy}
+  Question.all.each {|question| question.destroy}
+  redirect '/'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213170325) do
+ActiveRecord::Schema.define(version: 20161213192149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161213170325) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "answer"
   end
 
   create_table "surveys", force: :cascade do |t|
